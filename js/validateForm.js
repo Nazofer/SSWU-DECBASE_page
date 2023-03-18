2ececa6ec5f5eb357c0631f650f6b9db72df8a65
Nazar Vovk <nvovk.2004@gmail.com>
2023-03-19 01:54:13 +0200

    styled congrats-auth-modal

--- js/validateForm.js
+++ js/validateForm.js
@@ -4,8 +4,30 @@ const emailInput = form.querySelector('.form__input-email');
 const nameInput = document.querySelector('.form__input-text:first-child');
 const modal = document.querySelector('.form__modal');
 const closeModalBtn = document.querySelector('.form__modal-close');
+const congratsMessage = modal.querySelector('#congrats-auth-message');
 const nameRegex = /^[A-Z][a-z]*$/;
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/;
+const date = new Date();
+const year = date.toLocaleDateString('en-GB');
+
+const startConfetti = () => {
+  const confetti = new ConfettiGenerator({
+    target: 'my-canvas',
+    max: 80,
+    size: 1,
+    animate: true,
+    props: ['circle', 'square', 'triangle', 'line'],
+    colors: [
+      [255, 255, 255],
+      [255, 0, 0],
+      [0, 255, 0],
+      [0, 0, 255],
+    ],
+    clock: 25,
+    rotate: true,
+  });
+  confetti.render();
+};
 
 const validateInput = (input, regex, errorMessage) => {
   if (!regex.test(input.value.trim())) {
@@ -38,18 +60,24 @@ const validateInputs = (inputs) => {
   return isValid;
 };
 
-const addModalVisibility = () => modal.classList.add('form__modal--visible');
+const addModalVisibility = () => {
+  modal.classList.add('form__modal--visible');
+  startConfetti();
+};
 
-const removeModalVisibility = () =>
+const removeModalVisibility = () => {
   modal.classList.remove('form__modal--visible');
+  confetti.stop();
+};
 
 const showGreetingModal = () => {
   const userData = localStorage.getItem('userData');
   if (userData) {
     try {
-      const data = JSON.parse(userData);
-      const name = data.name;
+      const { name } = JSON.parse(userData);
       if (name === nameInput.value) {
+        congratsMessage.innerHTML = `Thanks for joining us, ${name}.<br>
+         You can use ${name.toUpperCase()}${year} promo to apply 12% discount!!!`;
         addModalVisibility();
         closeModalBtn.addEventListener('click', removeModalVisibility);
         setTimeout(removeModalVisibility, 5000);
