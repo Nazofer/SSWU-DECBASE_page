c4e7ef8e3408537d9843458cd4329542f765f279
Nazar Vovk <nvovk.2004@gmail.com>
2023-03-20 02:14:29 +0200

    uncomment alerting

--- js/alert.js
+++ js/alert.js
@@ -1,17 +1,17 @@
-// const eventList = ['click', 'scroll', 'selectionchange'];
-// const timeout = 60000;
+const eventList = ['click', 'scroll', 'selectionchange'];
+const timeout = 60000;
 
-// const resetTimer = () => {
-//   clearTimeout(timer);
-//   timer = setTimeout(showAlert, timeout);
-// };
+const resetTimer = () => {
+  clearTimeout(timer);
+  timer = setTimeout(showAlert, timeout);
+};
 
-// const showAlert = () => {
-//   confirm('Ви ще тут?') ? resetTimer() : window.close();
-// };
+const showAlert = () => {
+  confirm('Ви ще тут?') ? resetTimer() : window.close();
+};
 
-// eventList.forEach(event => {
-//   document.addEventListener(event, resetTimer);
-// });
+eventList.forEach(event => {
+  document.addEventListener(event, resetTimer);
+});
 
-// let timer = setTimeout(showAlert, timeout);
+let timer = setTimeout(showAlert, timeout);
